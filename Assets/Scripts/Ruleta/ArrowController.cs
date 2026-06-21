063e065e81383486d47a83e7fc6f5bae10094d2d
Arfeos <angelmartinezcastellanos1@gmail.com>
2026-06-21 16:30:24 +0200

    redimension del canvas

--- Assets/Scripts/Ruleta/ArrowController.cs
+++ Assets/Scripts/Ruleta/ArrowController.cs
@@ -11,9 +11,9 @@ public class ArrowController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        Debug.DrawRay(transform.position, Vector2.right*0.6f, Color.red, 1);
+        Debug.DrawRay(transform.position, Vector2.right*0.7f, Color.red, 1);
         if (rouletteService.GetRouletteStatus()) return; 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right , 0.6f, layerMask);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right , 0.7f, layerMask);
         if (hit)
         {
             if (hit.collider.GetComponent<SegmentController>() != null)

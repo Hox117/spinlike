f0cfa141a358ea620181158f6bb762fc3616915b
Hox117 <arraezgargabriel@gmail.com>
2026-06-26 11:54:59 +0200

    a

---
+++ Assets/Scripts/ExiButton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ExiButton : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Debug.Log("Saliendo del juego...");
+
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
